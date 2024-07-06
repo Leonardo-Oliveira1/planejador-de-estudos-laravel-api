6e996dbbe62053918799459d9a4e63b5e45f407d
Leonardo-Oliveira1 <oleonardo78@gmail.com>
2024-07-06 12:22:01 -0300

    feat ✨: adição do campo cor nos módulos pt. 2

--- app/Http/Controllers/ModulesController.php
+++ app/Http/Controllers/ModulesController.php
@@ -18,6 +18,7 @@ class ModulesController extends Controller
         $module = new Module;
         $module->user_id = auth()->user()['id'];
         $module->name = $params['name'];
+        $module->color = $params['color'];
         $module->save();
 
         return response()->json(['result' => "Matéria '".$params['name']."' adicionada!"]);
@@ -29,24 +30,27 @@ class ModulesController extends Controller
 
         $this->validateOwnerModule($id);
 
-        $module = Module::select('name', 'created_at', 'updated_at')->where('id', $request->input('id'))->first();
+        $module = Module::select('name', 'color', 'created_at', 'updated_at')->where('id', $request->input('id'))->first();
         return response()->json(['result' => $module]);
     }
 
     public function list(){
-        $module = Module::select('id', 'name', 'created_at', 'updated_at')->where('user_id', auth()->user()['id'])->get();
+        $module = Module::select('id', 'name', 'color', 'created_at', 'updated_at')->where('user_id', auth()->user()['id'])->get();
         return response()->json(['result' => $module]);
     }
     public function update(Request $request){
         $id = $request->input('id');
         $name = $request->input('name');
+        $color = $request->input('color');
         if(is_null($id)) throw new HttpResponseException(response()->json(['result' => 'O id da matéria é obrigatório.'], Response::HTTP_BAD_REQUEST));
         if(is_null($name)) throw new HttpResponseException(response()->json(['result' => 'O novo nome da matéria é obrigatório.'], Response::HTTP_BAD_REQUEST));
+        if(is_null($color)) throw new HttpResponseException(response()->json(['result' => 'Defina uma cor para esta matéria.'], Response::HTTP_BAD_REQUEST));
 
         $this->validateOwnerModule($id);
 
         $module = Module::find($id);
         $module->name = $name;
+        $module->color = $color;
         $module->save();
 
         return response()->json(['result' => 'Matéria alterada com sucesso!']);
@@ -68,11 +72,13 @@ class ModulesController extends Controller
 
     private function validateModule(Request $request){
         $name = $request->input('name');
+        $color = $request->input('color');
 
         if(is_null($name)) throw new HttpResponseException(response()->json(['result' => 'O nome da matéria é obrigatório.'], Response::HTTP_BAD_REQUEST));
+        if(is_null($color)) throw new HttpResponseException(response()->json(['result' => 'Defina uma cor para esta matéria.'], Response::HTTP_BAD_REQUEST));
         if(strlen($name) > 255) $name = substr($name, 0 , 255);
 
-        return ["name" => $name];
+        return ["name" => $name, "color" => $color];
     }
 
     private function validateOwnerModule($id){
